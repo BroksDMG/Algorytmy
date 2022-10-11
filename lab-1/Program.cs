e059fb77ff9cf443912857386fd3cab8ab5d0720
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-10-11 15:34:59 +0200

    Dodaj pliki projektów.

---
+++ lab-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 23, 1, 56, 345, 1, 5, 67, 11 };
+            int index = FindTwoDigitalMin(arr1);
+            if(index==7)
+            {
+                Console.WriteLine("Ok");
+
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            int[] arr2 = { };
+            index = FindTwoDidgitalMin(arr2);
+            if( index==-1)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            int[] arr3 = { 1, 3, 4, 123, 1234 };
+            index = FindTwoDigitalMin(arr3);
+            if(index == -1)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
