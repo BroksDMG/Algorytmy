76427869fac889f00835b07b91e94ec14dd220e3
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2022-11-08 15:44:24 +0100

    .

--- lab-4/Program.cs
+++ lab-4/Program.cs
@@ -1,133 +1,172 @@
 ﻿using System;
-
-namespace task_1
+namespace task_2
 {
-    public class Task1
+    public class Task2
     {
+
         public static void Main(string[] args)
         {
-            //task_1.1
-            Console.WriteLine("////////////////task_1.1/////////////////");
-            int[] arr = { 1, 2, 6 ,9 ,4, 3};
-            Console.WriteLine(IsInArrayRecursive(arr, 0, arr.Length - 1, 0));          //false
-            Console.WriteLine(IsInArrayRecursive(arr, 0, arr.Length - 1, 6));          //true
-            Console.WriteLine(IsInArrayRecursive(new int[]{}, 0, arr.Length - 1, 5));          //false
+            //task2
+            int[] arr1 = { 0, 2, 4, 6 };
+            int[] arr2 = { -2, -1, 10, 10_000, -1 };
+            //int[] arr3 = { 2, 1, 10, 10_000, 1 };
 
-            //task_1.2
-            Console.WriteLine("////////////////task_1.2/////////////////");
-            int[] arr1 = { 4, 5, 9, 4, 7, 2 };
-            Console.WriteLine(SumMod3(arr));
-            Console.WriteLine(SumMod3(arr1));
 
-            //task_1.3
-            Console.WriteLine("////////////////task_1.3/////////////////");
-            Console.WriteLine(Factorial(4));
+            Console.WriteLine(MinProduct(arr1));
+            Console.WriteLine(MinProduct(arr2));
+            //Console.WriteLine(MinProduct(arr3));
+            //task1
 
-            //task_1.4
-            Console.WriteLine("////////////////task_1.4/////////////////");
-            int[] arr2 = { 1, 3, 2, 8, 2 };
-            Console.WriteLine(IndexOfSumOfOthers(arr2));
+            int[,] gold = new int[,] { { 1, 3, 1, 5 },
+                                       { 2, 2, 4, 1 },
+                                       { 5, 0, 2, 3 },
+                                       { 0, 6, 1, 2 } };
+            Console.WriteLine(CollectGold(gold));
         }
-        public static bool IsInArray(int[] arr, int value)
+
+        /// <summary>
+        /// W tablicy gold znajdują się dodatnie liczby reprezetujące złoto. 
+        /// Górnik zbiera złoto z komórek, zaczyna od dowolnej komórki górnego wiersza (n) i 
+        /// i porusza się w dół do dolnego wiersza (0). Może przejść tylko do komórki po prawej lub
+        /// do komórki na przekątnej: w prawo w górę lub w prawo w dół, ale ostatecznie musi znaleźć się w dolnym wierszu (0).
+        /// Zaimplementuj algorytm, który wyznaczy największa liczbę złota zebraną przez górnika.
+        /// </summary>
+        /// <param name="gold">Dwuwymiarowa tablica liczb dodatnich</param>
+        /// <returns>Liczba zebranego złota</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        /// <summary>
+        // Przykłady
+        // Wejście: gold[][] = {{1, 3, 3},
+        //     {2, 1, 4},
+        //     {0, 6, 4}};
+        // Wyjście: 12
+        // {(1,0)->(2,1)->(1,2)}
+        //
+        // Wejście: gold[][] = { {1, 3, 1, 5},
+        //     {2, 2, 4, 1},
+        //     {5, 0, 2, 3},
+        //     {0, 6, 1, 2}};
+        // Wyjście: 16
+        //     (2,0) -> (1,1) -> (1,2) -> (0,3) LUB
+        //     (2,0) -> (3,1) -> (2,2) -> (2,3)
+        //
+        // Wejście : gold[][] = {{10, 33, 13, 15},
+        //     {22, 21, 04, 1},
+        //     {5, 0, 2, 3},
+        //     {0, 6, 14, 2}};
+        // Wyjście: 83 
+        // Uwaga!!!
+        // Najłatwiej zrealizować algorytm w postaci rekurencyjnej.
+        // Memoizacja zmniejszy złożoność czasową.
+        static public int CollectGold(int[,] gold)
         {
-            return IsInArrayRecursive(arr, 0, arr.Length, value);
-        }
-        /**
-         * REKURENCJA
-         */
-        //Zaimplementuj funkcję, która strategią dziel i zwyciężaj zwróci prawdę jeśli w tablicy
-        //'arr' znajduje się wartość parametru 'value'.
-        //Przykład
-        //int[] arr = { 1, 2, 6 ,9 ,4, 3};
-        //Console.WriteLine(IsInArrayRecursive(arr, 0, arr.Length - 1, 0);          //false
-        //Console.WriteLine(IsInArrayRecursive(arr, 0, arr.Length - 1, 6);          //true
-        //Console.WriteLine(IsInArrayRecursive(new int[]{}, 0, arr.Length - 1, 5);          //false
-        public static bool IsInArrayRecursive(int[] arr, int left, int right, int value)
-        {
-            if (arr.Length == 0)
+            int width = gold.GetLength(0);
+            int height = gold.GetLength(1);
+            int[,] memo = new int[width, height];
+            for (int i = 0; i < width; i++)
             {
-                return false;
+                for (int j = 0; j < height; j++)
+                {
+                    memo[i, j] = -1;
+                }
             }
-            else if (left > right)
+
+            int max = 0;
+            for (int i = 0; i < width; i++)
             {
-                return false;
+                max = Math.Max(max, CollectGoldHelper(gold, i, height - 1, memo));
             }
-            
-            int middle = (left + right) / 2;
-            if (arr[middle] == value){
-                    return true;
-                }
-            else if (arr[middle] > value){
-                    return IsInArrayRecursive(arr, left, middle - 1, value);
-                }
-            else{
-                    return IsInArrayRecursive(arr, middle + 1, right, value);
-                }
-            
-        }
 
-
-        //Zdefiniuj funkcję rekurecyjną, która oblicza sumę elementów tablicy podzielnych przez 3
-        //Nie można używać instrukcji iteracyjnych!!! Wartość funkcja dla pustej tablicy wynosi 0.
-        //Można założyć, że tablica nie będzie równa null. Zdefiniuj funkcję pomocniczą która będzie wywoływana
-        //rekurencyjnie wewnątrz SumMod3.
-        public static long SumMod3(int[] arr)
-        {
-            
-            return SumMod3Rec(arr, 0, arr.Length - 1);
+            return max;
         }
-        private static long SumMod3Rec(int[] arr, int firstNum, int secondNum)
+        static public int CollectGoldHelper(int[,] gold, int row, int col, int[,] memo)
         {
-            if (firstNum > secondNum)
+            if (col == 0)
             {
+                return gold[row, col];
+            }
+
+            if (memo[row, col] != -1)
+            {
+                return memo[row, col];
+            }
+
+            int max = 0;
+            for (int i = row - 1; i <= row + 1; i++)
+            {
+                if (i >= 0 && i < gold.GetLength(0))
+                {
+                    max = Math.Max(max, CollectGoldHelper(gold, i, col - 1, memo));
+                }
+            }
+
+            memo[row, col] = max + gold[row, col];
+            return memo[row, col];
+        }
+
+        /// <summary>
+        /// Zaimplementuj funkcję, która wyznaczy najmniejszy ilocz wybranych liczb z tablicy arr.
+        /// Iloczyn może składać się z jednej liczby.
+        /// Przykład 1
+        /// Wejscie: int[] arr = [0, 2, 4, 6]
+        /// Wyjście: 0
+        /// 
+        /// Przykład 2
+        /// Wejscie: int[] arr = [-2, -1, 10, 10_000, -1]
+        /// Wyjście: -200_000
+        /// 
+        /// Przykład 3
+        /// Wejscie: int[] arr = [2, 1, 10, 10_000, 1]
+        /// Wyjście: 1
+        /// </summary>
+        /// <param name="arr">tablica liczb całkowitych</param>
+        /// <returns>najmniejszy iloczyn tablicy wejściowej arr</returns>
+        static public int MinProduct(int[] a)
+        {
+            int n = a.Length;
+
+            if (n == 1)
+                return a[0];
+
+            int negmax = int.MinValue;
+            int posmin = int.MinValue;
+            int count_neg = 0, count_zero = 0;
+            int prod = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] == 0)
+                {
+                    count_zero++;
+                    continue;
+                }
+
+                if (a[i] < 0)
+                {
+                    count_neg++;
+                    negmax = Math.Max(negmax, a[i]);
+                }
+
+                if (a[i] > 0 && a[i] < posmin)
+                    posmin = a[i];
+                
+
+                prod *= a[i];
+            }
+
+            if (count_zero == n|| (count_neg == 0 && count_zero > 0))
                 return 0;
-            }
-            else
-            {
-                int middle = (firstNum + secondNum) / 2;
-                if (arr[middle] % 3 == 0)
-                {
-                    return arr[middle] + SumMod3Rec(arr, firstNum, middle - 1) + SumMod3Rec(arr, middle + 1, secondNum);
-                }
-                else
-                {
-                    return SumMod3Rec(arr, firstNum, middle - 1) + SumMod3Rec(arr, middle + 1, secondNum);
-                }
-            }
-        }
 
-        //Zdefiniuj funkcję rekurencyjną, która oblicza silnię liczby.
-        public static long Factorial(int n)
-        {
-            if (n <= 1)return 1;
-            else return (n * Factorial(n - 1));
-       
-        }
 
-        /**
-         * ALGORYTMY I ZŁOŻONOŚĆ
-         */
-        //Zdefiniuj funkcję, która zwróci indeks liczby, która jest równa sumie pozostałych elementów tablicy
-        //Przykład
-        //int[] arr = {1, 3, 2, 8, 2};
-        //int index = IndexOfSumOfOthers(arr);
-        //funkcja w `index` powinna zwrócić 3, gdyż pod tym indeksem jest 8 równe sumie 1 + 3 + 2 + 2.
-        //Jeśli w tablicy nie ma takiej liczby lub tablica jest pusta to funkcja pownna zwrócić -1.
-        public static int IndexOfSumOfOthers(int[] arr)
-        {
-            int sum = 0;
-            for (int i = 0; i < arr.Length; i++)
-            {
-                sum += arr[i];
-            }
-            for (int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] == sum - arr[i])
-                {
-                    return i;
-                }
-            }
-            return -1;
+            if (count_neg == 0)
+                return posmin;
+
+
+            if (count_neg % 2 == 0 && count_neg != 0)
+               prod = prod / negmax;
+           
+
+            return prod;
         }
     }
 }
